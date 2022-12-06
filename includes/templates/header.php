7db7b9d243d04784ea08b0b1d92fbb613bc8c2c7
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2022-12-06 00:27:15 -0300

    update files

---
+++ includes/templates/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="/bienes-raices-lautaroaguilar/build/css/app.css">
+</head>
+<body>
+    
+    <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/bienes-raices-lautaroaguilar/index.php">
+                    <img src="build/img/logo.svg" alt="Logotipo de bienes raices">
+                </a>
+
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                </div>
+
+                <div class="derecha">
+                    <img src="build/img/dark-mode.svg" alt="boton modo oscuro" class="dark-mode-boton">
+                    <nav class="navegacion">
+                        <a href="nosotros.php">Nosotros</a>
+                        <a href="anuncios.php">Anuncios</a>
+                        <a href="blog.php">Blog</a>
+                        <a href="contacto.php">Contacto</a>
+                    </nav>
+                </div>
+            </div> <!--Cierre de barra-->
+
+            <?php  echo $inicio ? "<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>" : ''; ?>
+        </div>
+    </header>
