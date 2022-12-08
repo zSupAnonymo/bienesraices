404443a35757b9549fc2f64216fc4c1c9705a9b3
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2022-12-08 00:00:13 -0300

    update files

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="/bienes-raices-lautaroaguilar/build/css/app.css">
+    <link rel="stylesheet" href="build/css/app.css">
 </head>
 <body>
     
