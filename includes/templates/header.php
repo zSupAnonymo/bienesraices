8742cd96dc7eb4067aa74ff2f59720c6bd5e16ec
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2022-12-13 00:13:17 -0300

    update files

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 <body>
     
@@ -13,20 +13,20 @@
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/bienes-raices-lautaroaguilar/index.php">
-                    <img src="build/img/logo.svg" alt="Logotipo de bienes raices">
+                    <img src="/build/img/logo.svg" alt="Logotipo de bienes raices">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                    <img src="/build/img/barras.svg" alt="icono menu responsive">
                 </div>
 
                 <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="boton modo oscuro" class="dark-mode-boton">
+                    <img src="/build/img/dark-mode.svg" alt="boton modo oscuro" class="dark-mode-boton">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros.php">Nosotros</a>
+                        <a href="/anuncios.php">Anuncios</a>
+                        <a href="/blog.php">Blog</a>
+                        <a href="/contacto.php">Contacto</a>
                     </nav>
                 </div>
             </div> <!--Cierre de barra-->
